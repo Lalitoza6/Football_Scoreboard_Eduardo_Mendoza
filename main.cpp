dc81fdd4cce36501764b39667f6b47149e7eee86
Lalitoza6 <>
2021-04-22 00:43:37 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,33 @@
+//Eduardo Mendoza
+//Computer Science 2
+//4-24-2021
+//Dr. T
+
+
+#include <iostream>
+
+using namespace std;
+
+class Team{
+
+private: 
+int Score,Timeout_Count;
+ string Name,Home_City,Coach_Name;
+ bool Home_Status;
+
+public:
+ Team(){
+Name = "Team Name";
+Coach_Name = "Coach name";
+Home_City = "Hometown";
+Home_Status = false;
+Score = 0;
+Timeout_Count = 0;
+ }
+//setter and getter start here
+void 
+class Scoreboard{
+
+int main (){
+  Scoreboard Sc;
+ }
